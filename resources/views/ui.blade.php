40dc66b33935b0d81e8a032e12a03c1d7bfbce25
unknown <sheilayong99@gmail.com>
2020-06-18 21:21:30 +0800

    add files

---
+++ resources/views/ui.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+</head>
+<body> 
+
+<div class="container mt-3">
+  <div class="d-flex flex-column mb-3">
+    <div class="p-2 bg-white"  style="  border: 2px solid;
+  border-bottom: 0px; border-left: 0px; border-right: 0px;">
+      <div class="row">
+        <div class="col-sm-8">WMC Tech Sdn Bhd<br>webinaryincetan.com</div>
+        
+        <div class="col-sm-4"><img src="https://lh3.googleusercontent.com/92SW3Li-	hpkaQw52-Nw0ow784TLj3f0kmAro80CxuN7S1eDw2XyEgpUAqLa3KwOssj_2xA=s85" class="rounded" alt="Cinque Terre"></div>
+      </div>
+    </div>
+    
+    
+    <div class="p-2 bg-dark text-white">PLAN<br/>CLICKFUNNERLS PLATINUM ($297/MO AFTER 14-DAY TRIAL)</div>
+    <div class="p-2 bg-dark text-white">VISITS</div>
+    <div class="p-2 bg-dark text-white">PAGES</div>
+    <div class="p-2 bg-dark text-white">FUNNELS</div>
+    
+  </div>
+
+  <div class="d-flex flex-column mb-3">
+    <div class="p-2 bg-white" style="border: 2px solid;">
+      Get Advanced Training<br>FUNNEL HACKS<br><button type="button" class="btn btn-primary">Watch Webinar Now</button>
+    </div>
+  </div>
+
+  <div class="d-flex flex-column mb-3">
+    <div class="p-2 bg-white" style="border: 2px solid;">Want Advanced Funnel Training?
+    </div>
+</div>
+</div>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/milktea', function () {
-    return view('Thank You');
+Route::get('/ui', function () {
+    return view('ui');
 });
+
